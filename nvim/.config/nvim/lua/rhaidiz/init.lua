69e96ccc54d0a62e0ad669b5c32013afe4e03025
rhaidiz <rhaidiz@outlook.com>
2022-05-02 10:36:01 +0200

    Add diagnostic on cursor hover

--- nvim/.config/nvim/lua/rhaidiz/init.lua
+++ nvim/.config/nvim/lua/rhaidiz/init.lua
@@ -2,3 +2,8 @@ require("rhaidiz.telescope")
 require("rhaidiz.lsp_config")
 require("rhaidiz._cmp")
 require("bufferline").setup{}
+
+-- Show line diagnostics automatically in hover window
+vim.o.updatetime = 250
+vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
