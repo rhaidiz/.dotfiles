5c23e4e693f4824fc1f47c957889368b85b81d50
Federico De Meo <me@rhaidiz.net>
2024-07-24 00:03:12 -0400

    fix(nvim): fix pyenv initialization for ssh connections

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,7 @@
 if status is-interactive; and set -q SSH_CONNECTION; and not set -q TMUX
-	tmux new-session -A -s (hostname)
+  tmux new-session -A -s (hostname)
+else
+  pyenv init - | source
 end
 
 # disable fish greeting
@@ -7,5 +9,4 @@ set fish_greeting
 
 set -x HOMEBREW_NO_AUTO_UPDATE 1
 # starship init fish | source
-pyenv init - | source
 
