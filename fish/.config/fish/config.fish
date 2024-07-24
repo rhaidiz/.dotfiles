5e26802500916c74f39e5b8155f69cb996cb3b1e
Federico De Meo <me@rhaidiz.net>
2024-07-24 00:11:38 -0400

    fix(fish): remove pyenv for now

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,5 @@
 if status is-interactive; and set -q SSH_CONNECTION; and not set -q TMUX
   tmux new-session -A -s (hostname)
-else
-  pyenv init - | source
 end
 
 # disable fish greeting
@@ -9,4 +7,3 @@ set fish_greeting
 
 set -x HOMEBREW_NO_AUTO_UPDATE 1
 # starship init fish | source
-
